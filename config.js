7a3238b50e6a72e648a8fbad6d0ee034e303b747
novice <seungyeon5374@gmail.com>
2024-06-12 17:08:03 +0900

    몽고 아틀라스 연동

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports = {
-    MONGODB_URL : process.env.MONGODB_URL,
     JWT_SECRET : process.env.JWT_SECRET,
     PORT : process.env.PORT
 }
