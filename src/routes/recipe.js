7d53a9ec6bfad5a880e2d4ef3368faa50fea2a03
novice <seungyeon5374@gmail.com>
2024-06-13 12:53:25 +0900

    경로 오류 찾기

--- src/routes/recipe.js
+++ src/routes/recipe.js
@@ -12,7 +12,7 @@ const Review = require('../models/Review')
 const upload = multer({
     storage: multer.diskStorage({
         destination: function( req, file, cb){
-            cb(null,'./uploads/')
+            cb(null,'uploads/')
         },
         filename: function(req, file, cb){
             const ext = path.extname(file.originalname)
