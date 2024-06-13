279672ccc0de48a4d14e2d66cfe9c30e726b40af
novice <seungyeon5374@gmail.com>
2024-06-13 13:10:11 +0900

    경로찾기

--- src/routes/users.js
+++ src/routes/users.js
@@ -11,7 +11,7 @@ const Recipe = require('../models/Recipe')
 const upload = multer({
     storage: multer.diskStorage({
         destination: function( req, file, cb){
-            cb(null,'public/uploads/')
+            cb(null,'./uploads/')
         },
         filename: function(req, file, cb){
             const ext = path.extname(file.originalname)
