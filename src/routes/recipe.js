24cd55712c523aecaf560df83c1af892e5f5e0a7
novice <seungyeon5374@gmail.com>
2024-06-17 16:12:56 +0900

    사진 저장이름 지정

--- src/routes/recipe.js
+++ src/routes/recipe.js
@@ -11,6 +11,8 @@ const AWS = require('aws-sdk')
 const multer = require('multer')
 const multerS3 = require('multer-s3')
 const path = require('path')
+const {v4: uuidv4} = require('uuid')
+const crypto = require('crypto')
 
 AWS.config.update({
     accessKeyId:process.env.AWS_ACCESS_KEY_ID,
@@ -25,10 +27,10 @@ const upload = multer({
         acl:'public-read',
         contentType: multerS3.AUTO_CONTENT_TYPE,
         key(req,file,cb){
-            cb(null,`cookingImgs/${Date.now()}_${path.basename(file.originalname)}`)
+            cb(null,`cookingImgs/${crypto.createHash('sha256').update(uuidv4()).digest('hex')}`)
         },
     }),
-    limits:{fileSize: 5 * 1024 * 1024 },
+    limits:{fileSize: 2 * 1024 * 1024 },
 })
 const upload2 = multer()
 
